ee06c6e1604fd5b1b262efbb454439fa0036a4fa
Hoilc Zhang <hoilc@foxmail.com>
2026-03-30 03:08:17 +0800

    feat(manifest): add pikpak

---
+++ manifests/pikpak/script.ps1
@@ -0,0 +1,6 @@
+$url = 'https://download.mypikpak.net/desktop/official_PikPak.exe'
+
+$info = Extract-VersionInfoFromRemoteFile $url -PreferVersionField "ProductVersion"
+Merge-State $info
+
+$State.compareMode = 'semver'
