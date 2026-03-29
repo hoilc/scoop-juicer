360c2e7a62fff4257bf818274bb858017e24b279
Hoilc Zhang <hoilc@foxmail.com>
2026-03-30 02:24:51 +0800

    feat(manifest): update filesystemanalyzer

--- manifests/filesystemanalyzer/script.ps1
+++ manifests/filesystemanalyzer/script.ps1
@@ -1,17 +1,4 @@
 $url = 'https://dmitrybrant.com/files/FileSystemAnalyzer.zip'
-$exePath = 'FileSystemAnalyzer.exe'
 
-$archiverName = Split-Path -Path $url -Leaf
-$exeName = Split-Path -Path $exePath -Leaf
-
-$TempFolder = New-TempFolder
-$TempArchiver = Join-Path $TempFolder $archiverName
-$TempExe = Join-Path $TempFolder $exePath
-
-Invoke-WebRequest -Uri $url -OutFile $TempArchiver
-Expand-7zArchive -Path $TempArchiver -DestinationPath $TempFolder -Include $exePath
-
-$State.version = Read-ProductVersionFromExe -FilePath $TempExe
+$State.version = Extract-VersionFromRemoteFileProductVersion $url -ExtractFilePath 'FileSystemAnalyzer.exe'
 $State.compareMode = 'semver'
-
-Remove-Item -Path $TempFolder -Recurse -Force
