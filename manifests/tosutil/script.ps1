91d63fd875767e14809cea3e3788621c50e24350
Hoilc Zhang <hoilc@foxmail.com>
2026-03-30 02:23:56 +0800

    feat(manifest): add tosutil

---
+++ manifests/tosutil/script.ps1
@@ -0,0 +1,5 @@
+$url = 'https://tos-tools.tos-cn-beijing.volces.com/windows/tosutil#/tosutil.exe'
+
+$State.version = Extract-VersionFromRemoteFileExec $url -Argument 'version' -Regex 'version: v([\d.]+)'
+$State.compareMode = 'semver'
+
