ff72a88bf241d81deb72789b017dc4d795492c4f
Hoilc Zhang <hoilc@foxmail.com>
2026-03-30 02:24:38 +0800

    feat(manifest): update slimpdfreader

--- manifests/slimpdfreader/script.ps1
+++ manifests/slimpdfreader/script.ps1
@@ -1,13 +1,5 @@
 $url = 'https://cdn.investintech.com/download/InstallSlimPDFReader.exe'
 
-$archiverName = Split-Path -Path $url -Leaf
-
-$TempFolder = New-TempFolder
-$TempArchiver = Join-Path $TempFolder $archiverName
-
-Invoke-WebRequest -Uri $url -OutFile $TempArchiver
-
-$State.version = Read-FileVersionFromExe -FilePath $TempArchiver
+$State.version = Extract-VersionFromRemoteFileVersion $url
 $State.compareMode = 'semver'
 
-Remove-Item -Path $TempFolder -Recurse -Force
