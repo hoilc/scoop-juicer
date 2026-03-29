26e1556895f860aeb39d7e36d106d1beea894af8
Hoilc Zhang <hoilc@foxmail.com>
2026-03-30 03:39:07 +0800

    feat(manifest): update obsutil

--- manifests/obsutil/script.ps1
+++ manifests/obsutil/script.ps1
@@ -1,12 +1,5 @@
 $url = 'https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_windows_amd64.zip'
 
-$tempFile = Get-RemoteFile $url
-$filelist = Get-7zArchiveList $tempFile
-
-if ($filelist -match 'obsutil_windows_amd64_([\d.]+)') {
-    $State.version = $matches[1]
-} else {
-    throw "cannot find version in file list"
-}
+$State.version = Extract-VersionFromRemoteZipFileList $url -Regex 'obsutil_windows_amd64_([\d.]+)'
 
 $State.compareMode = 'semver'
