553dcd1ea1f46f3d27a99e61d72c16d17ce8f291
Hoilc Zhang <hoilc@foxmail.com>
2026-03-30 12:37:23 +0800

    feat(manifest): update pikpak

--- manifests/pikpak/script.ps1
+++ manifests/pikpak/script.ps1
@@ -1,4 +1,4 @@
 $url = 'https://download.mypikpak.net/desktop/official_PikPak.exe'
 
-$info = Extract-VersionInfoFromRemoteFile $url -PreferVersionField "ProductVersion"
+$info = Extract-VersionInfoFromRemotePeFile $url -PreferVersionField "ProductVersion"
 Merge-State $info
