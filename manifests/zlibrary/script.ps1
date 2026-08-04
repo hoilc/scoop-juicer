00c33b4ef644d4b8c96c14b01be912b4e85a596f
Hoilc Zhang <hoilc@foxmail.com>
2026-08-05 00:27:12 +0800

    manifest(zlibrary): remove as no longer needed

--- manifests/zlibrary/script.ps1
+++
@@ -1,4 +0,0 @@
-$url = 'https://s3proxy-alp.cdn-zlib.sk/swfs_second_public_files/soft/desktop/Z-Library-latest.exe'
-
-$info = Extract-VersionInfoFromRemotePeFile $url -PreferVersionField "ProductVersion"
-Merge-State $info
